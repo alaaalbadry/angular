72d02e8657b69adbb2baba91880abd339d9a17be
navras <moamen.zakaria@gmail.com>
2020-05-03 21:33:44 +0200

    created the 'piece of news' model

---
+++ src/app/model/piece-of-news.model.ts
@@ -0,0 +1,12 @@
+export class PieceOfNews {
+
+    ID: number;
+    DepartmentName: string;
+    Title: string;
+    ImagePath: string;
+    ImagePathThumb: string;
+    Date: string;
+    Time: string;
+    ArabicDate: string;
+
+}
