08232d4923fb9ad225e10d8fbabe508abb8ed6f6
alaa <alaa2gmail.com>
2020-05-05 01:41:37 +0200

    added dao service and getNewsDetailsById()

---
+++ src/app/services/dao.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DaoService {
+
+  constructor(private apiService:ApiService) { }
+
+  getNewDetailsById(newId:number)
+  {
+   return this.apiService.get(`/News/GetDetails?id=`+newId);
+  }
+}
