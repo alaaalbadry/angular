ba7a1886c5d0801e829a4780a2f3366d09d73786
alaa <alaa2gmail.com>
2020-05-04 15:18:02 +0200

    refactor sidebar *ngFor

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,11 +8,31 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 export class SidebarComponent implements OnInit {
 
   @Input() type=1;
+  MoreNews:string[]=[];
+  RecentNews:string[]=[];
+  news:string[]=[];
   constructor() { }
 
   ngOnInit(): void {
+    this.setNewsType();
   }
 
+  setNewsType():any{
+    if(this.type===1){
+      this.RecentNews.push("الخبر الاول");
+      this.RecentNews.push("الخبر الثاني ");
+      this.RecentNews.push("الخبر الثالث");
+      this.news=this.RecentNews;
+      
+    }else
+    {
+      this.MoreNews.push("الخبرالخامس");
+      this.MoreNews.push("الخبر الرابع ");
+      this.MoreNews.push("الخبر السادس");
+      this.news=this.MoreNews;
+      
+    }
+  }
   getNewsType():any{
     if(this.type===1){
       return "اضيف حديثا";
