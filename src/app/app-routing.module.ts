2598bba4318ee437ef24a37dfe8ae82c7bd6b76d
alaa <alaa2gmail.com>
2020-05-05 01:07:58 +0200

    added router path for details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PieceOfNewsDetailsComponent } from './components/piece-of-news-details/piece-of-news-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'details/:id',component:PieceOfNewsDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
