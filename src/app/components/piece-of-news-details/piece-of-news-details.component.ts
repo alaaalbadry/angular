53b3400e11e43234141f434690623c7c02b02cc1
alaa <alaa2gmail.com>
2020-05-05 02:24:56 +0200

    added details.ts implementation

--- src/app/components/piece-of-news-details/piece-of-news-details.component.ts
+++ src/app/components/piece-of-news-details/piece-of-news-details.component.ts
@@ -1,4 +1,7 @@
+import { ResponseViewDetails } from './../../model/responseViewDetails.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DaoService } from 'src/app/services/dao.service';
 
 @Component({
   selector: 'piece-of-news-details',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PieceOfNewsDetailsComponent implements OnInit {
 
-  constructor() { }
+  newDetails:ResponseViewDetails;
+  id: number = 0;
+  constructor(private _activatedRoute: ActivatedRoute, private daoService: DaoService) { }
 
   ngOnInit(): void {
+    this._activatedRoute.paramMap.subscribe(params => {
+      this.id = + params.get('id');
+      this.getNewDetails(this.id);
+    });
+  }
+
+  getNewDetails(newId: number) {
+    this.daoService.getNewDetailsById(newId).subscribe((response) => {
+
+      if (response.Success) {
+        this.newDetails=response.Data;
+      }
+      else {
+        alert(response.Message);
+      }
+    });
+
+
   }
 
 }
