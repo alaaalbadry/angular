e67f14e305619a7619196cf2c6a7c55d46607d49
navras <moamen.zakaria@gmail.com>
2020-05-05 03:38:12 +0200

    news preview dynamic implementation

--- src/app/components/news-small-preview/news-small-preview.component.ts
+++ src/app/components/news-small-preview/news-small-preview.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DaoService } from 'src/app/services/dao.service';
+import { ResponseViewDetails } from './../../model/responseViewDetails.model';
 
 @Component({
   selector: 'news-small-preview',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsSmallPreviewComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  id: number;
+
+  newsDetails:ResponseViewDetails = new ResponseViewDetails();
+
+  constructor(private daoService: DaoService) { }
 
   ngOnInit(): void {
+
+    this.getNewsDetails();
+
+  }
+
+  getNewsDetails(): void {
+
+    this.daoService.getNewDetailsById(this.id).subscribe(response=>{
+
+      this.newsDetails =  response.Data;
+
+    });
+
   }
 
 }
